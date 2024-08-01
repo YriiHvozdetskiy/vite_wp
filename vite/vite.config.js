4031c75fdf39a1f3be7a7932de378f8a8ac90818
rasty <hvozdetskiyyrii@gmail.com>
2024-08-01 14:26:19 +0300

    update

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -16,32 +16,57 @@ function getPagesInput() {
    return pages;
 }
 
-export default defineConfig({
-   plugins: [
-      Inspect(),
-      handlebars({
-         partialDirectory: resolve(__dirname, 'src/templates'),
-         helpers: {
-            array: (...items) => items.slice(0, -1),
-            hash: (context) => context.hash,
-         },
-         context: (pagePath) => ({
-            companyName: 'Your Company Name',
-            currentYear: new Date().getFullYear(),
+export default defineConfig(({command, mode}) => {
+   const isProd = mode === 'production';
+
+   return {
+      plugins: [
+         Inspect(),
+         handlebars({
+            partialDirectory: resolve(__dirname, 'src/templates'),
+            helpers: {
+               array: (...items) => items.slice(0, -1),
+               hash: (context) => context.hash,
+            },
+            context: (pagePath) => ({
+               companyName: 'Your Company Name',
+               currentYear: new Date().getFullYear(),
+            }),
          }),
-      }),
-   ],
-   resolve: {
-      alias: {
-         '@': resolve(__dirname, 'src'),
+      ],
+      resolve: {
+         alias: {
+            '@': resolve(__dirname, 'src'),
+         },
       },
-   },
-   build: {
-      rollupOptions: {
-         input: getPagesInput(),
+      build: {
+         outDir: resolve(__dirname, '../themes/project_name/dist'),
+         emptyOutDir: true,
+         rollupOptions: {
+            input: {
+               main: resolve(__dirname, 'main.js'),
+               ...getPagesInput(),
+            },
+            output: {
+               entryFileNames: `assets/[name]${isProd ? '' : ''}.js`,
+               chunkFileNames: `assets/[name]${isProd ? '' : ''}.js`,
+               assetFileNames: (assetInfo) => {
+                  if (assetInfo.name.endsWith('.css')) {
+                     return `assets/[name]${isProd ? '' : ''}[extname]`;
+                  }
+                  return `assets/[name][extname]`;
+               }
+            }
+         },
+         minify: isProd,
+         cssMinify: isProd,
+         sourcemap: !isProd,
       },
-   },
-   server: {
-      open: '/pages/dashboard.html',
-   },
+      server: {
+         watch: {
+            usePolling: true,
+         },
+         open: '/pages/dashboard.html',
+      }
+   }
 });
